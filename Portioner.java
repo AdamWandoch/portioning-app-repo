5534a52d2ab8021c66852e5838f15994fbd24fec
Adam Wandoch <wandoch.adam@gmail.com>
2022-01-15 12:48:08 +0000

    some cleanup and formatting

--- Portioner.java
+++ Portioner.java
@@ -2,12 +2,10 @@
 package portioningapp;
 
 /**
- *
  * @author AdamWandoch
  */
-public class Portioner 
-{
- 
+public class Portioner {
+
     private double currentFillet;
     private int targetPortions = 0;
     private double target;
@@ -20,28 +18,27 @@ public class Portioner
     private double totalFilletsWeight = 0;
     private double totalVolumeFromGrader = 0;
     private double maxMixGradeSize = 0;
-    
+
     private double targetGradeMin;
     private double targetGradeMax;
     private int targetGradeCount;
-    
+
     private double narrowGradeSmallMin;
     private double narrowGradeSmallMax;
     private double narrowGradeBigMin;
     private double narrowGradeBigMax;
-    private int narrowGradeSmallCount; 
+    private int narrowGradeSmallCount;
     private int narrowGradeBigCount;
-    
+
     private double wideGradeSmallMin;
     private double wideGradeSmallMax;
     private double wideGradeBigMin;
     private double wideGradeBigMax;
     private int wideGradeSmallCount;
     private int wideGradeBigCount;
-    
-    
-    public Portioner(double targetSize)
-    {
+
+
+    public Portioner(double targetSize) {
         target = targetSize;
         targetGradeMin = target * 0.96;
         targetGradeMax = target * 1.04;
@@ -54,9 +51,8 @@ public class Portioner
         wideGradeBigMin = target * 1.1;
         wideGradeBigMax = target * 1.16;
     }
-    
-    public static double round(double value, int places) 
-    {
+
+    public static double round(double value, int places) {
         if (places < 0) throw new IllegalArgumentException();
 
         long factor = (long) Math.pow(10, places);
@@ -64,30 +60,29 @@ public class Portioner
         long tmp = Math.round(value);
         return (double) tmp / factor;
     }
-    
-    public void PrintStats()
-    {
+
+    public void PrintStats() {
         int gradedCount = narrowGradeSmallCount + narrowGradeBigCount + wideGradeSmallCount + wideGradeBigCount + targetGradeCount;
         double gradedAmount = totalFilletsWeight - mixGradeAmount;
-        System.out.println("Total amount processed: " + (int)totalFilletsWeight);
-        System.out.println("Total volume from grader: " + (int)totalVolumeFromGrader);
+        System.out.println("Total amount processed: " + (int) totalFilletsWeight);
+        System.out.println("Total volume from grader: " + (int) totalVolumeFromGrader);
         System.out.println("Total graded count: " + gradedCount);
-        System.out.println("Average graded portion size: " + (gradedAmount / gradedCount));
-        System.out.println("Product yield: " + (gradedAmount / totalFilletsWeight));
-        System.out.println("Target cuts percentage: " + ((double)targetGradeCount) / ((double)gradedCount));
+        System.out.println("Average graded portion size: " + String.format("%.5f", (gradedAmount / gradedCount)));
+        System.out.println("Product yield: " + String.format("%.5f", (gradedAmount / totalFilletsWeight)));
+        System.out.println("Target cuts percentage: " + String.format("%.5f", ((double) targetGradeCount) / ((double) gradedCount)));
         System.out.println("target count: " + targetGradeCount);
         System.out.println("narrowSmalls: " + narrowGradeSmallCount);
         System.out.println("narrowBigs: " + narrowGradeBigCount);
         System.out.println("wideSmalls: " + wideGradeSmallCount);
         System.out.println("wideBigs: " + wideGradeBigCount);
         System.out.println("mixGrade: " + mixGradeCount);
-        System.out.println("Mix amount: " + (int)mixGradeAmount);
-        System.out.println("Mix percentage: " + mixGradeAmount / totalFilletsWeight);
-        System.out.println("Average mix grade size: " + mixGradeAmount / mixGradeCount);
+        System.out.println("Mix amount: " + (int) mixGradeAmount);
+        System.out.println("Mix percentage: " + String.format("%.5f", mixGradeAmount / totalFilletsWeight));
+        System.out.println("Average mix grade size: " + String.format("%.5f", mixGradeAmount / mixGradeCount));
         System.out.println("Max mix grade size: " + round(maxMixGradeSize, 5));
         System.out.println(" ");
     }
-    
+
     // ------------ PORTIONER ALGORYTHM SIMULATING REGULAR PORTIONING WITH SELF-BALANCE ------------------------------- //
     // ----------------------------------- WITH ADDED LAST-CUT-GROWTH ------------------------------------------------- //
     // ********************************* AND FULL DOUBLE OFFCUT OPTIMIZATION ****************************************** //
@@ -95,73 +90,60 @@ public class Portioner
     // ******************* !!!! GRADER is still called too many times during process  !!!!! *************************** //
     // ******************* the difference between totalVolumeFromGrader and totalFillets weight************************ //
     // ******************* is in multiplies of target size; means that grade(target) called to many times************** //
-    public void process(double fillet)
-    {
-        
+    public void process(double fillet) {
+
         currentFillet = fillet;
         totalFilletsWeight += currentFillet;                // update the total amount of fillets processed
-        
-                                                            // if current fillet is big enough to give us priorityCut
-                                                            // and if needed apply the priority cut first       
-        
-        if (priorityCut <= currentFillet && priorityCut > 0) 
-        {
+
+        // if current fillet is big enough to give us priorityCut
+        // and if needed apply the priority cut first
+
+        if (priorityCut <= currentFillet && priorityCut > 0) {
             grade(priorityCut);
             currentFillet -= priorityCut;                   // take the priority cut off current fillet
         }
         priorityCut = 0;
-        
-                                                            // if current fillet is big enough to give us second priority cut
-                                                            // then if needed apply the priority cut2
-        
-        if (priorityCut2 <= currentFillet && priorityCut2 > 0) 
-        {
+
+        // if current fillet is big enough to give us second priority cut
+        // then if needed apply the priority cut2
+
+        if (priorityCut2 <= currentFillet && priorityCut2 > 0) {
             grade(priorityCut2);
             currentFillet -= priorityCut2;                  // take the second priority cut off current fillet
         }
         priorityCut2 = 0;
-        
-        targetPortions = (int)(currentFillet / target);     // calculate how many taget cuts can you cut from current fillet
+
+        targetPortions = (int) (currentFillet / target);     // calculate how many taget cuts can you cut from current fillet
         offcut = currentFillet - (targetPortions * target); // calculate the size of the offcut after cutting off target portions
-        
-        if (currentFillet >= wideGradeSmallMin)
-        {
-            if (offcut <= target * 0.16)                     // if the offcut is smaller or equal to biggest accepted growth size then grow the last cut and cut it off
-            {
+
+        if (currentFillet >= wideGradeSmallMin) {
+            if (offcut <= target * 0.16) {                    // if the offcut is smaller or equal to biggest accepted growth size then grow the last cut and cut it off
                 grade(target + offcut);
-                for (int i = 0; i < targetPortions - 1; i++) // and cut the rest for target sized portions
-                {
+                for (int i = 0; i < targetPortions - 1; i++) { // and cut the rest for target sized portions
                     grade(target);
                 }
                 priorityCut = (target * 2) - (offcut + target); // then update priority cut to match with the grown cut
-            } else if (offcut >= wideGradeSmallMin)             // if the offcut is bigger or equal to smallest gradable portion then grade it on its own
-            {
+            } else if (offcut >= wideGradeSmallMin) {            // if the offcut is bigger or equal to smallest gradable portion then grade it on its own
                 grade(offcut);
-                for (int i = 0; i < targetPortions; i++)        // then cut the rest for target portions
-                {
-                    grade(target); 
+                for (int i = 0; i < targetPortions; i++) {       // then cut the rest for target portions
+                    grade(target);
                 }
                 priorityCut = (target * 2) - offcut;        // and update the priority cut to match the offcut portion
-            } else                                          // otherwise (if offcut too big to grow the portion but too small to be graded on its own)
-            {
-                if (offcut <= target * 0.32)                // if offcut is big enough to grow two porions  (2 * 0.16 or less) then
-                {
+            } else {                                         // otherwise (if offcut too big to grow the portion but too small to be graded on its own)
+                if (offcut <= target * 0.32) {               // if offcut is big enough to grow two porions  (2 * 0.16 or less) then
                     grade(target * 1.16);                   // grow one to the max
                     offcut -= target * 0.16;                // find out how much offcut is left
                     grade(target + offcut);                 // grow the second one with the rest of the offcut
-                    for (int i = 0; i < targetPortions - 2; i++) // grade the rest for target sized cuts
-                    {
+                    for (int i = 0; i < targetPortions - 2; i++) { // grade the rest for target sized cuts
                         grade(target);
                     }
                     priorityCut = (target * 2) - (target * 1.16);
                     priorityCut2 = (target * 2) - (target + offcut);
                     offcut = 0;
-                } else                                      // if offcut is bigger than doubled max growth size so > 2 * 0.16
-                {
+                } else {                                     // if offcut is bigger than doubled max growth size so > 2 * 0.16
                     grade(target * 1.16);                   // it is needed twice for sure!!
                     grade(target * 1.16);                   // grade two grown portions 
-                    for (int i = 0; i < targetPortions - 2; i++) // grade the rest for target sized cuts
-                    {
+                    for (int i = 0; i < targetPortions - 2; i++) { // grade the rest for target sized cuts
                         grade(target);
                     }
                     grade(offcut - (target * 0.32));        // grade the rest of offcut (for mix, wasted)
@@ -170,15 +152,13 @@ public class Portioner
                 }
 
             }
-        } else
-        {
+        } else {
             grade(currentFillet);
-        }   
+        }
     }
-    
-       
-    private void grade(double portion)
-    {
+
+
+    private void grade(double portion) {
         /*//testing break mechanism start
         if (portion > currentFillet) 
         {
@@ -187,29 +167,22 @@ public class Portioner
         this.currentFillet -= portion;
          //testing break mechanism end (makes no sense actually, portion likely to be > currentFillet)*/
         totalVolumeFromGrader += portion;
-        if (portion >= wideGradeSmallMin && portion < wideGradeSmallMax)
-        {
+        if (portion >= wideGradeSmallMin && portion < wideGradeSmallMax) {
             wideGradeSmallCount += 1;
-        } else if (portion >= narrowGradeSmallMin && portion < narrowGradeSmallMax)
-        {
+        } else if (portion >= narrowGradeSmallMin && portion < narrowGradeSmallMax) {
             narrowGradeSmallCount += 1;
-        } else if (portion >= targetGradeMin && portion < targetGradeMax)
-        {
+        } else if (portion >= targetGradeMin && portion < targetGradeMax) {
             targetGradeCount += 1;
-        } else if (portion >= narrowGradeBigMin && portion < narrowGradeBigMax)
-        {
+        } else if (portion >= narrowGradeBigMin && portion < narrowGradeBigMax) {
             narrowGradeBigCount += 1;
-        } else if (portion >= wideGradeBigMin && portion <= wideGradeBigMax)
-        {
+        } else if (portion >= wideGradeBigMin && portion <= wideGradeBigMax) {
             wideGradeBigCount += 1;
-        } else if (portion < wideGradeSmallMin)
-        {
+        } else if (portion < wideGradeSmallMin) {
             mixGradeCount += 1;
             mixGradeAmount += portion;
-            if (portion > maxMixGradeSize)
-            {
+            if (portion > maxMixGradeSize) {
                 maxMixGradeSize = portion;
-            } 
-        } 
-    }   
+            }
+        }
+    }
 }
--- PortioningApp.java
+++ PortioningApp.java
@@ -2,63 +2,39 @@
 package portioningapp;
 
 /**
- *
  * @author AdamWandoch
  */
-public class PortioningApp
-{
+public class PortioningApp {
 
     /**
      * @param args the command line arguments
      */
-    
-    public static void main(String[] args)
-    {
+
+    private static int testCount = 0;
+    private static double portionSize = 125;
+    private static int testSize = 200;
+    private static double randomFillet;
+    private static double minFilletSize = 450;
+    private static double maxFilletSize = 900;
+
+    public static void main(String[] args) {
         System.out.println("    // ------------ PORTIONER ALGORYTHM SIMULATING REGULAR PORTIONING WITH SELF-BALANCE ------------------------------- //\n" +
-"    // ----------------------------------- WITH ADDED LAST-CUT-GROWTH ------------------------------------------------- //\n" +
-"    // ********************************* AND FULL DOUBLE OFFCUT OPTIMIZATION ****************************************** //");
-        double portionSize = 125;
-        int testSize = 200;
-        double randomFillet;
-        double minFilletSize = 450;
-        double maxFilletSize = 900;
-        
-        System.out.println("Test One: ");
-        Portioner machine1 = new Portioner(portionSize);
-        
-        for (int i = 0; i < testSize; i++)
-        {
-            randomFillet = 500;//Math.random() * (maxFilletSize - minFilletSize) + minFilletSize;
-            machine1.process(randomFillet);
-        }
-        machine1.PrintStats();
-        //machine1 = null;
-        //////////////////////////////////////////////////////
-        
-        System.out.println("Test Two: ");
-        Portioner machine2 = new Portioner(portionSize);
-        
-        for (int i = 0; i < testSize; i++)
-        {
-            randomFillet = Math.random() * (maxFilletSize - minFilletSize) + minFilletSize;
-            machine2.process(randomFillet);
-        }
-        machine2.PrintStats();
-        //machine2 = null;
-        //////////////////////////////////////////////////////
-        
-        System.out.println("Test Three: ");
-        Portioner machine3 = new Portioner(portionSize);
-        
-        for (int i = 0; i < testSize; i++)
-        {
-            randomFillet = Math.random() * (maxFilletSize - minFilletSize) + minFilletSize;
-            machine3.process(randomFillet);
-        }
-        machine3.PrintStats();
-        //machine3 = null;
-        //////////////////////////////////////////////////////
-        
+                "    // ----------------------------------- WITH ADDED LAST-CUT-GROWTH ------------------------------------------------- //\n" +
+                "    // ********************************* AND FULL DOUBLE OFFCUT OPTIMIZATION ****************************************** //");
+
+        runTest();
+        runTest();
+        runTest();
     }
-    
+
+    public static void runTest() {
+        testCount++;
+        System.out.println("Test " + testCount + ": ");
+        Portioner machine = new Portioner(portionSize);
+        for (int i = 0; i < testSize; i++) {
+            randomFillet = Math.random() * (maxFilletSize - minFilletSize) + minFilletSize;
+            machine.process(randomFillet);
+        }
+        machine.PrintStats();
+    }
 }
